b925f5a95013d11ea6fc41f8e903946be4225e3c
TarunRV18 <149991425+TarunRV18@users.noreply.github.com>
2024-10-05 09:38:33 +0530

    Add files via upload

---
+++ abstract.java
@@ -0,0 +1,29 @@
+public class abstract {
+    abstract class Animal {
+        public abstract void sound();
+    public void sleep(){
+        System.out.println(x:"the animal is sleeping");
+    }
+
+publuc class Dog extends Animal{
+    public void sound();{
+        System.out.println(x:"the dog bark");
+    }
+}
+public class cat extends Animal{
+    public class sound(){
+        System.out.println(x:"cat moves");
+    }
+}
+public class abstract{
+    public static void main(String[] args) {
+        Animalmy myDog=now Animal;
+        Animal myCat=new cat();
+
+        myDog.sound();
+        myCat.sound();
+
+        myDog.sleep();
+        myCat.sleep();
+        }
+}
